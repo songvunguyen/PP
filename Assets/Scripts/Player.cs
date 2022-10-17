8c1fc44082498e064219089804014026be29da55
Song Vu Nguyen <vunnems08@gmail.com>
2022-10-17 13:35:09 -0400

    Add Movement to player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    Vector2 moveVal;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // rb.position += speed * Time.deltaTime * moveVal;
+        rb.velocity = new Vector2(moveVal.x * speed, moveVal.y * speed);
+    }
+
+    void OnMove(InputValue value){
+        moveVal = value.Get<Vector2>();
+    }
+}
