72fc68d14f8c4057af05e4309c218a39c2fa3e5e
Song Vu Nguyen <vunnems08@gmail.com>
2022-10-24 12:58:23 -0400

    Added bouncing physic for the ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     Rigidbody2D rb;
-    public float speed = 100.0f;
+    public float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +19,48 @@ public class Ball : MonoBehaviour
         
     }
 
+    //Adding reflect directions to the ball
+    // Using math code from https://noobtuts.com/unity/2d-pong-game
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight) {
+        // ascii art:
+        // ||  1 <- at the top of the racket
+        // ||
+        // ||  0 <- at the middle of the racket
+        // ||
+        // || -1 <- at the bottom of the racket
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+
     private void OnCollisionEnter2D(Collision2D other) {
-        
+        // Note: 'col' holds the collision information. If the
+        // Ball collided with a racket, then:
+        //   col.gameObject is the racket
+        //   col.transform.position is the racket's position
+        //   col.collider is the racket's collider
+
+        // Hit the left Racket?
+        if (other.gameObject.tag == "RacketLeft") {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position, other.transform.position, other.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            // Set Velocity with dir * speed
+            rb.velocity = dir * speed;
+        }
+
+        // Hit the right Racket?
+        if (other.gameObject.tag == "RacketRight") {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position, other.transform.position, other.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            // Set Velocity with dir * speed
+            rb.velocity = dir * speed;
+        }
     }
 }
