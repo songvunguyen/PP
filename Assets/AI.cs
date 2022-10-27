81b0adb374d20fbc06c085274a89709b5f4a3eb9
Song Vu Nguyen <vunnems08@gmail.com>
2022-10-27 13:21:35 -0400

    Added AI to the game

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class AI : MonoBehaviour
 {
+    public float speed = 10f;
+    Rigidbody2D rb;
+    public Ball b;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if((transform.position.y - b.transform.position.y) < 0){
+            rb.velocity = new Vector2(0, speed);
+        }else if((transform.position.y - b.transform.position.y) > 0){
+            rb.velocity = new Vector2(0, -speed);
+        }
+          
     }
+
 }
