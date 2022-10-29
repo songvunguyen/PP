03e02beae861ca909a14d563af06afcab919f54c
Song Vu Nguyen <vunnems08@gmail.com>
2022-10-29 18:14:33 -0400

    Fixed buttons and improve AI

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AI : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 5f;
     Rigidbody2D rb;
     public Ball b;
     // Start is called before the first frame update
@@ -15,12 +15,13 @@ public class AI : MonoBehaviour
 
     void Update()
     {
-        if((transform.position.y - b.transform.position.y) < 0){
-            rb.velocity = new Vector2(0, speed);
-        }else if((transform.position.y - b.transform.position.y) > 0){
-            rb.velocity = new Vector2(0, -speed);
-        }
-          
+        if(b.transform.position.x > 0){
+            if((transform.position.y - b.transform.position.y) < 0){
+                rb.velocity = new Vector2(0, speed);
+            }else if((transform.position.y - b.transform.position.y) > 0){
+                rb.velocity = new Vector2(0, -speed);
+            }
+        }     
     }
 
 }
