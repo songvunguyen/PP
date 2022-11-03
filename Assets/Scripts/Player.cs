172dd994d5285df7f470be92f10a4ed6c4cecb18
Song Vu Nguyen <vunnems08@gmail.com>
2022-11-03 17:30:37 -0400

    Change player control back to old input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,12 +17,30 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         // rb.position += speed * Time.deltaTime * moveVal;
-        rb.velocity = new Vector2(moveVal.x * speed, moveVal.y * speed);
+        // rb.velocity = new Vector2(moveVal.x * speed, moveVal.y * speed);
+        
+        if(this.name == "Player"){
+            if(Input.GetKey(KeyCode.W)){
+                rb.velocity = new Vector2(0, speed);
+            }else if(Input.GetKey(KeyCode.S)){
+                rb.velocity = new Vector2(0, -speed);
+            }else{
+                rb.velocity = Vector2.zero;
+            }
+        }else if(this.name == "Player2"){
+            if(Input.GetKey(KeyCode.UpArrow)){
+                rb.velocity = new Vector2(0, speed);
+            }else if(Input.GetKey(KeyCode.DownArrow)){
+                rb.velocity = new Vector2(0, -speed);
+            }else{
+                rb.velocity = Vector2.zero;
+            }
+        }
     }
 
-    void OnMove(InputValue value){
-        moveVal = value.Get<Vector2>();
-    }
+    // void OnMove(InputValue value){
+    //     moveVal = value.Get<Vector2>();
+    // }
 
     
 }
